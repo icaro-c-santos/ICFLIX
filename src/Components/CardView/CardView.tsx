7078fee53fbc45c5fdaccbb24cd5e36f2571e9df
ICARO SANTOS <icarosalna@hotmail.com>
2023-01-23 20:18:22 -0300

    create backdrop

--- src/Components/CardView/CardView.tsx
+++ src/Components/CardView/CardView.tsx
@@ -5,7 +5,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { Chip } from "@mui/material";
+import { Backdrop, backdropClasses, Chip } from "@mui/material";
 import { useState } from "react";
 import useEnhancedEffect from "@mui/material/utils/useEnhancedEffect";
 import { useEffect } from "react";
@@ -42,11 +42,11 @@ const themeCardClosed = {
 
 const themeCardOpen = {
   position: "fixed",
-  top:"20px",
+  top: "20px",
   width: 400,
   padding: "20px 20px 0px 20px",
   textAlign: "center",
-  cursor:"pointer",
+  cursor: "pointer",
   border: "solid",
   borderRadius: "10px",
   zIndex: 1000,
@@ -66,44 +66,52 @@ export default function CardView(cardProps: TCardViewProps) {
   const handlerClickCardView = () => {
     setIsOpen(!isOpen);
   };
-  return (
-    <Card sx={theme} onClick={handlerClickCardView}>
-      <CardMedia
-        sx={{
-          height: 200,
-          width: 200,
-          backgroundSize: "contain",
-          borderRadius: "10px",
-          margin: "auto",
-        }}
-        image={cardProps.image}
-        title={cardProps.name}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {cardProps?.name}
-        </Typography>
 
-        <Chip
-          size="small"
-          label={cardProps.status}
+  return (
+    <>
+      <Backdrop open={isOpen} ></Backdrop>
+      <Card sx={theme} onClick={handlerClickCardView}>
+        <CardMedia
           sx={{
-            color: "white",
-            bgcolor: statusColor[cardProps.status || "unknown"],
+            height: 200,
+            width: 200,
+            backgroundSize: "contain",
+            borderRadius: "10px",
+            margin: "auto",
           }}
-        ></Chip>
-        {isOpen && (
-          <CardContent>
-            <ul
-              style={{ listStyleType: "none", fontSize: "24px", lineHeight: 2 }}
-            >
-              {cardProps.species && <li>Especie: {cardProps.species}</li>}
-              {cardProps.gender && <li>Gênero: {cardProps.gender}</li>}
-              {cardProps.location && <li>Origem: {cardProps.location}</li>}
-            </ul>
-          </CardContent>
-        )}
-      </CardContent>
-    </Card>
+          image={cardProps.image}
+          title={cardProps.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {cardProps?.name}
+          </Typography>
+
+          <Chip
+            size="small"
+            label={cardProps.status}
+            sx={{
+              color: "white",
+              bgcolor: statusColor[cardProps.status || "unknown"],
+            }}
+          ></Chip>
+          {isOpen && (
+            <CardContent>
+              <ul
+                style={{
+                  listStyleType: "none",
+                  fontSize: "24px",
+                  lineHeight: 2,
+                }}
+              >
+                {cardProps.species && <li>Especie: {cardProps.species}</li>}
+                {cardProps.gender && <li>Gênero: {cardProps.gender}</li>}
+                {cardProps.location && <li>Origem: {cardProps.location}</li>}
+              </ul>
+            </CardContent>
+          )}
+        </CardContent>
+      </Card>
+    </>
   );
 }
