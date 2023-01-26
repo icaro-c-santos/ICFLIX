69ad954194f45ea97191a57235ddca1251d38392
ICARO SANTOS <icarosalna@hotmail.com>
2023-01-25 22:26:46 -0300

    adjust bug

--- src/Routers/ProtectedRouters.tsx
+++ src/Routers/ProtectedRouters.tsx
@@ -1,38 +1,14 @@
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../Context/AuthContext";
 import { Navigate, useNavigate } from "react-router-dom";
+import { Login } from "../pages/Login/Login";
 
-export type TAuthData = {
-  login?: string;
-  token?: string;
-  refreshToken?: string;
-  name?: string;
-  avatarUrl?: string;
-  isLoggedIn?: boolean;
-};
-
-export const ProtectedRouter = ({
+export const ProtectedRouter =  ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
-  const { userLogged, setUserLogged } = useContext(AuthContext);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (userLogged.isLoggedIn != null) {
-      return;
-    }
-    const data = localStorage.getItem("userLogged");
-    if (data) {
-      const user: TAuthData = JSON.parse(data);
-      user.isLoggedIn && setUserLogged(user);
-      return
-    }
-    if (userLogged.isLoggedIn != true) {
-      navigate("/login");
-    }
-  }, []);
-
-  return <>{children}</>;
+  const { userLogged } = useContext(AuthContext);
+ 
+  return userLogged.isLoggedIn?  <>{children}</> : <Login/> ;
 };
