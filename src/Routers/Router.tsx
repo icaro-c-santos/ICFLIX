04ab1f72c0e081c5b1d8ca75c57efb036fcbb63b
ICARO SANTOS <icarosalna@hotmail.com>
2023-01-26 23:22:23 -0300

    update Browser router to HashRouter

--- src/Routers/Router.tsx
+++ src/Routers/Router.tsx
@@ -3,7 +3,7 @@ import { Home } from "../pages/Home/Home";
 import { About } from "../pages/About/About";
 import { Contact } from "../pages/Contact/Contact";
 import { Characters } from "../pages/Characters/Characters";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { ProtectedRouter } from "./ProtectedRouters";
 import { AuthProvider } from "../Context/AuthContext";
 import { Login } from "../pages/Login/Login";
